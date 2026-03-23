31bd3e4bec8dc285091f4699df7b2879912d8c5f
MUHAMMAD MALFIN <malvin.arkade7@gmail.com>
2026-03-23 11:46:12 +0700

    feat: initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button"
 
 
 function App() {
@@ -5,6 +6,7 @@ function App() {
   return (
     <>
       <h1 className="text-red-500">Hello World</h1>
+      <Button>Click</Button>
     </>
   )
 }
